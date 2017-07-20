aecee236dbdcf4e051a094792cdc491ca87ef272
Rissy Lin <lsj.ruihan@gmail.com>
2017-07-20 22:34:53 +0800

    1) Spring Security LDAP 2) Access Facebook data

---
+++ src/main/java/com/example/springboot/facebook/FacebookApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot.facebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/springboot/ldap/LDAPApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LDAPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LDAPApplication.class, args);
+    }
+
+}
